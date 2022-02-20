3bcc30c24a509671ec1b615ec3d71cd79586c082
Johan Quijano <johanqnms@gmail.com>
2022-02-20 08:55:35 -0500

    Ajustes - Profile Responsive

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,16 +3,23 @@ import profile from './../../assets/img/profile.jpg'
 import react from './../../assets/img/reactjs.jpg'
 import angular from './../../assets/img/angular.png'
 import spring from './../../assets/img/spring.png'
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
+
 
 export const Profile = () => {
     return (
         <div className='profile-container'>
             <div className='profile-description'>
-                <h1 className='profile-title'>Johan Quijano</h1>
-                <h2 className='profile-subtitle'>Ingeniero de Sistemas, SCRUM Master</h2>
-                <img className='profile-image' src={profile} alt='Profile'/> 
-                <p className='profile-description'>Cuento con más de 12 años de experiencia trabajando en proyectos de tecnología me han permitido conocer todo tipo de sectores como: Comercio electrónico, empresarial, editorial, impresiones, comunicaciones, logístico y el sector público</p>
-                <span className='profile-icons'>Iconos</span>
+                <h1 className='profile-title'>Johan <br></br>Quijano</h1>
+                <h3 className='profile-subtitle'>Ingeniero de Sistemas<br></br> SCRUM Master</h3>
+                <div className='profile-image'><img  src={profile} alt='Profile'/></div> 
+                <p className='profile-description' align="justify">Cuento con más de 12 años de experiencia trabajando en proyectos de tecnología me han permitido conocer todo tipo de sectores como: Comercio electrónico, empresarial, editorial, impresiones, comunicaciones, logístico y el sector público</p>
+                <span className='profile-icons'>
+                <BsLinkedin /> <BsGithub /><MdOutlineEmail />
+                </span>
+               
             </div>
             
             <div className='images'>
